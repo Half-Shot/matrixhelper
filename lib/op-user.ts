a832c78b8da40682607e68bd1de70adbc743d00e
Will Hunt <will@half-shot.uk>
2021-05-12 10:55:28 +0100

    Add join to op user script

--- lib/op-user.ts
+++ lib/op-user.ts
@@ -2,7 +2,13 @@ import { getASClientFromEnv } from "./helpers/util";
 
 async function main() {
     const roomId = process.env.MX_ROOM_ID;
+    const via = process.env.MX_ROOM_VIA;
+    const power = parseInt(process.env.USER_POWER);
+    if (Number.isNaN(power) || power < 0 || !Number.isSafeInteger(power)) {
+        throw Error('Number was not a positive integer, rejecting');
+    }
     const client = getASClientFromEnv();
+    await client.joinRoom(roomId, via.split(","));
     await client.setUserPowerLevel(process.env.USER_TO_POWER, roomId, parseInt(process.env.USER_POWER));
 }
 
