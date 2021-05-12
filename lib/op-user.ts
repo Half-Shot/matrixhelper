69568b1ec64d61b60e5c4c2b157d8985881f3d05
Will Hunt <will@half-shot.uk>
2021-05-12 11:15:56 +0100

    handle case of a nonjoined room

--- lib/op-user.ts
+++ lib/op-user.ts
@@ -2,14 +2,20 @@ import { getASClientFromEnv } from "./helpers/util";
 
 async function main() {
     const roomId = process.env.MX_ROOM_ID;
-    const via = process.env.MX_ROOM_VIA;
+    const via = process.env.MX_ROOM_VIA || roomId.split(':')[1];
     const power = parseInt(process.env.USER_POWER);
     if (Number.isNaN(power) || power < 0 || !Number.isSafeInteger(power)) {
         throw Error('Number was not a positive integer, rejecting');
     }
     const client = getASClientFromEnv();
-    await client.joinRoom(roomId, via.split(","));
+    const isJoined = (await client.getJoinedRooms()).includes(roomId);
+    if (!isJoined) {
+        await client.joinRoom(roomId, via.split(","));
+    }
     await client.setUserPowerLevel(process.env.USER_TO_POWER, roomId, parseInt(process.env.USER_POWER));
+    if (!isJoined) {
+        await client.leaveRoom(roomId);
+    }
 }
 
 
