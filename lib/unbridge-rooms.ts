bd4cdf35a7ab120e45202d80e7a7bf15bb190adb
Will Hunt <willh@matrix.org>
2021-06-10 14:13:59 +0100

    Tweak unbridge-rooms

--- lib/unbridge-rooms.ts
+++ lib/unbridge-rooms.ts
@@ -2,37 +2,42 @@ import { MatrixClient } from "matrix-bot-sdk";
 import { getASClientFromEnv, getClientFromEnv } from "./helpers/util";
 import { createInterface } from "readline";
 
-const USER_REGEX = /@gitter_\:matrix\.org/;
+const USER_REGEX = new RegExp(process.env.USER_REGEX);
 const live = process.env.DRY === 'false';
 
 async function main() {
-    const rl = createInterface({
-        input: process.stdin,
-        terminal: false,
-        crlfDelay: 500,
-    });
     if (live) {
         console.log(`Doing a LIVE run`);
     } else {
         console.log(`Doing a dry run`);
     }
+    const rl = createInterface({
+        input: process.stdin,
+        terminal: false,
+        crlfDelay: 500,
+    });
     const bridgeClient = getClientFromEnv(true);
     for await (const alias of rl) {
         try {
+            await bridgeClient.joinRoom(alias, ['libera.ems.host']);
             await handleRoom(bridgeClient, alias);
         } catch (ex) {
-            console.error(`${alias}: ERRO Failed to handle ${ex.message}`);
+            console.error(`${alias}: ERROR Failed to handle ${ex.message}`);
         }
     }
     // Also handle any rooms the bot is still in
-    const roomsTheBotIsIn = await bridgeClient.getJoinedRooms();
-    for (const roomId of roomsTheBotIsIn) {
-        try {
-            await handleRoom(bridgeClient, roomId);
-        } catch (ex) {
-            console.error(`${roomId}: ERRO Failed to handle`, ex);
-        }
-    }
+    // const roomsTheBotIsIn = await bridgeClient.getJoinedRooms();
+    // for (const roomId of roomsTheBotIsIn) {
+    //     if ((await bridgeClient.getJoinedRoomMembers(roomId)).length <= 4) {
+    //         console.log(`Skipping ${roomId} as it has less than two users`);
+    //         continue;
+    //     }
+    //     try {
+    //         await handleRoom(bridgeClient, roomId);
+    //     } catch (ex) {
+    //         console.error(`${roomId}: ERRO Failed to handle`, ex);
+    //     }
+    // }
 }
 
 async function handleRoom(bridgeClient: MatrixClient, roomIdOrAlias: string) {
@@ -40,21 +45,22 @@ async function handleRoom(bridgeClient: MatrixClient, roomIdOrAlias: string) {
     const log = (type: "log"|"error"|"warn", ...args: any) => console[type](`${roomIdOrAlias}: `,...args);
     // First, resolve the alias
     const roomId: string = await bridgeClient.resolveRoom(roomIdOrAlias);
-    try {
-        live ? await bridgeClient.setDirectoryVisibility(roomId, "private") : log("log", "would have set visibility");
-        log("log", "Set visibility to private");
-    } catch (ex) {
-        log("warn", "Could not set visibility", ex.message);
-    }
 
-    if (roomIdOrAlias.startsWith("#")) {
-        try {
-            live ? await bridgeClient.deleteRoomAlias(roomIdOrAlias) : log("log", "would have removed the alias");
-            log("log", `Set removed alias from ${roomId}`)
-        } catch (ex) {
-            log("error", "Could not remove alias", ex.message);
-        }
-    }
+    // try {
+    //     live ? await bridgeClient.setDirectoryVisibility(roomId, "private") : log("log", "would have set visibility");
+    //     log("log", "Set visibility to private");
+    // } catch (ex) {
+    //     log("warn", "Could not set visibility", ex.message);
+    // }
+
+    // if (roomIdOrAlias.startsWith("#")) {
+    //     try {
+    //         live ? await bridgeClient.deleteRoomAlias(roomIdOrAlias) : log("log", "would have removed the alias");
+    //         log("log", `Set removed alias from ${roomId}`)
+    //     } catch (ex) {
+    //         log("error", "Could not remove alias", ex.message);
+    //     }
+    // }
 
     // Drop PLs
     const canDropPls = await bridgeClient.userHasPowerLevelFor(myId, roomId, "m.room.power_levels", true);
@@ -65,15 +71,20 @@ async function handleRoom(bridgeClient: MatrixClient, roomIdOrAlias: string) {
                 delete pls.users[user];
             }
         }
-        live ? await bridgeClient.sendStateEvent(roomId, "m.room.power_levels", "", pls) : log("log", "would changed the PLs to", pls);
-        log("log", `Set powerlevels`);
+        if (pls.users.length > 0) {
+            log("log", `Set powerlevels`);
+            live ? await bridgeClient.sendStateEvent(roomId, "m.room.power_levels", "", pls) : log("log", "would changed the PLs to", pls);
+        } else {
+            log("log", "Not setting PLs, room would have no powered users");
+        }
     } else {
         log("warn", "Bot doesn't have the powerlevel to drop PLs here");
     }
 
-    // Drop gitter users in the room
+    // Drop users in the room
     try {
         const members = (await bridgeClient.getJoinedRoomMembers(roomId)).filter(r => r.match(USER_REGEX));
+        console.log(`Got ${members.length} members`, members);
         for (const bridgeMember of members) {
             try {
                 log("log", `Leaving ${bridgeMember}`);
