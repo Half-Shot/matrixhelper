4c9f3128420b0693bee68402908be49053faa7ec
Will Hunt <will@half-shot.uk>
2024-04-15 09:45:37 +0100

    move logic.

--- lib/legacyify-rooms-irc.ts
+++ lib/legacyify-rooms-irc.ts
@@ -1,4 +1,4 @@
-import { LogService, PowerLevelsEventContent } from "matrix-bot-sdk";
+import { LogService, MatrixClient, PowerLevelsEventContent, RoomNameEventContent } from "matrix-bot-sdk";
 import { getClientFromEnv } from "./helpers/util";
 import { createInterface } from "readline/promises";
 import postgres from "postgres";
@@ -30,7 +30,7 @@ async function main() {
     console.log(`Found ${bridgeRooms.length} portal rooms`);
     console.log(`Found ${provisionRooms.length} plumbed rooms`);
 
-    for await (const roomId of [...dmRooms, ...bridgeRooms]) {
+    for await (const roomId of [...provisionRooms]) {
         let plcontent: PowerLevelsEventContent;
         try {
             plcontent = await client.getRoomStateEvent(roomId, "m.room.power_levels", "");
@@ -46,65 +46,27 @@ async function main() {
             continue;
         }
         if (plcontent.events_default !== 100) {
-            if (Envs.dry) {
-                console.log('Would set PL in room to 100')
-            } else {
-                plcontent.events_default = 100;
-                if (process.env.BRIDGE_MESSAGE) {
-                    await client.sendMessage(roomId, process.env.BRIDGE_MESSAGE);
-                }
-                await client.sendStateEvent(roomId, "m.room.power_levels", "", plcontent);
-            }
+            await makeRoomLegacy(client, roomId, plcontent);
         }
         stats.completed++;
+        // Do *one* room.
+        return;
     }
-    Object.entries(stats).forEach(([stat, value]) => console.log(`${stat}: ${value}`));
 }
 
-// async function makeRoomLegacy(client: MatrixClient, roomId: string, name: string, alias: string) {
-//     const userId = await client.getUserId();
-//     // First, join the room.
-//     await client.joinRoom(roomId);
-//     // Now check the PLs
-//     const plcontent = await client.getRoomStateEvent(roomId, "m.room.power_levels", "");
-//     if (plcontent.users[userId] !== 100) {
-//         process.stdout.write("Applying admin permissions...");
-//         const members = await client.getJoinedRoomMembers(roomId);
-//         const adminUserID = Object.entries(plcontent.users).find(([userId, power]) => power === 100 && members.includes(userId))[0];
-//         const adminClient = getASClientFromEnv(adminUserID);
-//         await adminClient.sendStateEvent(roomId, "m.room.power_levels", "", {
-//             ...plcontent,
-//             users: {
-//                 ...plcontent.users,
-//                 [userId]: 100,
-//             }
-//         });
-//         // First, get admin in the room.
-//         console.log("DONE");
-//     }
-//     try {
-//         process.stdout.write("Deleting alias...");
-//         // Then remove the alias
-//         await client.deleteRoomAlias(alias);
-//         console.log("DONE");
-//     } catch (ex) {
-//         if (ex.body.errcode === 'M_NOT_FOUND') {
-//             console.warn(`IGNORE No alias found. ignoring`);
-//         }
-//     }
-//     process.stdout.write("Set directory visiblity...");
-//     await client.setDirectoryVisibility(roomId, "private");
-//     console.log("DONE");
-//     if (!name.startsWith("[OBSOLETE]")) {
-//         process.stdout.write("Changing room name");
-//         // Then, set the room name to something else
-//         await client.sendStateEvent(roomId, "m.room.name", "", {name: `[OBSOLETE] ${name}`});
-//         console.log("DONE");
-//         process.stdout.write("Sending notice...");
-//         await client.sendText(roomId, "This room is being rebridged. Please check the room directory soon for the new room");
-//         console.log("DONE");
-//     }
-// }
+async function makeRoomLegacy(client: MatrixClient, roomId: string, plcontent: PowerLevelsEventContent) {
+    if (Envs.dry) {
+        console.log('Would set PL in room to 100')
+    } else {
+        plcontent.events_default = 100;
+        if (process.env.BRIDGE_MESSAGE) {
+            await client.sendMessage(roomId, process.env.BRIDGE_MESSAGE);
+        }
+        const { name } = await client.getRoomStateEvent(roomId, "m.room.name", "") as RoomNameEventContent;
+        await client.sendStateEvent(roomId, "m.room.name", "", { name: `[DISABLED] ${name}`});
+        await client.sendStateEvent(roomId, "m.room.power_levels", "", plcontent);
+    }
+}
 
 main().catch((ex) => {
     console.log("Failed to run command:", ex.toString());
